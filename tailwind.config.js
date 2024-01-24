9999d3c036be17f69160dec14aaec94cefe20c0b
Areeb Siddiqui <16635847+Areeb2016@users.noreply.github.com>
2024-01-24 19:56:30 +0500

    fixed responsive issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     "node_modules/preline/dist/*.js",
   ],
   theme: {
+    screens: {
+      xs: "0px",
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [prelinePlugin],
